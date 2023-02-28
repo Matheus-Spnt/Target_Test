e700bab5ec84cab8cb9ca69ea2d3e047a9fdc8cd
Matheus Barriento <matheus.barriento01@gmail.com>
2023-02-28 12:11:16 -0300

    Opening Project

---
+++ Target_Test_prj/Target_Test_prj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Target_Test_prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
